ad5989fa9663fbb0f0be49f77d679f36f2ed8a77
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-12-02 17:57:12 +0100

    Uploading files

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,42 @@
 require('dotenv').config()
 const path = require('path')
+const { v4: uuidv4 } = require('uuid')
+
 const express = require('express')
 const bodyPaser = require('body-parser')
 const mongoose = require('mongoose')
+const multer = require('multer')
 
 const feedRoutes = require('./routes/feed')
 
 const app = express()
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images')
+	},
+	filename: (req, file, cb) => {
+		cb(null, uuidv4() + '-' + file.originalname)
+	}
+})
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true)
+	} else {
+		cb(null, false)
+	}
+}
+
 // app.use(bodyPaser.urlencoded()) // x-www-form-urlencoded <form>
 app.use(bodyPaser.json()) // application/json
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+)
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 // CORS error handling
