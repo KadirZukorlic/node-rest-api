249da35871bc0f44f5dfcdfe2d4c89e891b66b5e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-12-02 16:59:18 +0100

    Handle errors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const bodyPaser = require('body-parser')
 const mongoose = require('mongoose')
@@ -9,6 +10,7 @@ const app = express()
 
 // app.use(bodyPaser.urlencoded()) // x-www-form-urlencoded <form>
 app.use(bodyPaser.json()) // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 // CORS error handling
 app.use((req, res, next) => {
@@ -19,6 +21,14 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes)
+
+app.use((error, req, res, next) => {
+	console.log(error)
+	const status = error.statusCode || 500
+	const message = error.message
+	const data = error.data
+	res.status(status).json({ message: message, data: data })
+})
 
 mongoose
 	.connect(process.env.MONGODB_CONNECT_URI)
