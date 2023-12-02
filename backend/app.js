81e41f635b11e5c6d4a511b9990d994755824177
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-12-02 03:05:38 +0100

    Add dotenv and connect to the mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const bodyPaser = require('body-parser')
+const mongoose = require('mongoose')
 
 const feedRoutes = require('./routes/feed')
 
@@ -18,4 +20,10 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes)
 
-app.listen(8080)
+mongoose
+	.connect(process.env.MONGODB_CONNECT_URI)
+	.then(() => {
+		console.log('Connected to MongoDB')
+		app.listen(8080)
+	})
+	.catch((err) => console.log(err))
