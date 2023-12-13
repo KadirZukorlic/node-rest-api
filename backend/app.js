aee86e30d7460df707f8a012541c1e4749c3e21a
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-12-13 01:41:37 +0100

    Establish socket connection

--- backend/app.js
+++ backend/app.js
@@ -63,6 +63,10 @@ mongoose
 	.connect(process.env.MONGODB_CONNECT_URI)
 	.then(() => {
 		console.log('Connected to MongoDB')
-		app.listen(8080)
+		const server = app.listen(8080)
+		const io = require('socket.io')(server)
+		io.on('connection', (socket) => {
+			console.log('Client connected')
+		})
 	})
 	.catch((err) => console.log(err))
